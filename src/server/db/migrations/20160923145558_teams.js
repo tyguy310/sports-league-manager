88f3bdc004a64a022d100604b603ca4897f5c3a4
Tyler Maier <tyler_maier@yahoo.com>
2016-09-23 17:54:41 -0600

    migrate teams and events

--- src/server/db/migrations/20160923145558_teams.js
+++ src/server/db/migrations/20160923145558_teams.js
@@ -7,10 +7,8 @@ exports.up = function(knex, Promise) {
     table.string('zip').notNullable().unique();
     table.string('gender').notNullable();
     table.boolean('coed').notNullable();
-    table.integer('users_id').notNullable();
-    table.foreign('users_id').references('id').inTable('users');
-    table.integer('users_id').notNullable();
-    table.foreign('users_id').references('id').inTable('users');
+    table.integer('players_id').notNullable();
+    table.foreign('players_id').references('id').inTable('players');
     table.integer('sports_id').notNullable();
     table.foreign('sports_id').references('id').inTable('sports');
   });
