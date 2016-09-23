5ed3684a915fbbea7f4ac5b5467e73b099d1f46f
Allen Fordham <allen.s.fordham@gmail.com>
2016-09-23 15:11:19 -0600

    added location table and faker seed, added faker to node modules

--- src/server/db/migrations/20160923145547_locations.js
+++ src/server/db/migrations/20160923145547_locations.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('locations', (table) => {
+    table.increments();
+    table.string('location_name').notNullable();
+    table.string('location_image');
+    table.string('location_description');
+    table.string('loc_add_line_1').notNullable();
+    table.string('loc_add_line_2');
+    table.string('loc_add_city').notNullable();
+    table.string('loc_add_state').notNullable();
+    table.string('loc_add_zip').notNullable();
+    table.string('league_id').references('leagues.id');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('locations');
 };
