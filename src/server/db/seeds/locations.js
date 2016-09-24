05032346942a06cb724dd1a1fff3e7ed44976d3c
Heathcliff <dennis.nathan@gmail.com>
2016-09-24 12:01:04 -0600

    Uncommented files that I had commented out for testing purposes

--- src/server/db/seeds/locations.js
+++ src/server/db/seeds/locations.js
@@ -1,24 +1,24 @@
-// const faker = require('faker');
-//
-// function locationsSeed(knex) {
-//   return knex('locations').insert({
-//     location_name: faker.random.locale(),
-//     location_image: faker.image.sports(),
-//     location_description: faker.lorem.sentence(),
-//     loc_add_line_1: faker.address.streetAddress(),
-//     loc_add_city: faker.address.city(),
-//     loc_add_state: faker.address.state(),
-//     loc_add_zip: faker.address.zipCode().split('-')[0],
-//   });
-// }
-//
-// exports.seed = function (knex, Promise) {
-//   const iterationArray = new Array(100);
-//
-//   var ArrayOfPromises = Array.from(iterationArray)
-//   .map(() => {
-//     return addressSeed(knex);
-//   });
-//
-//   return Promise.all(ArrayOfPromises);
-// };
+const faker = require('faker');
+
+function locationsSeed(knex) {
+  return knex('locations').insert({
+    location_name: faker.random.locale(),
+    location_image: faker.image.sports(),
+    location_description: faker.lorem.sentence(),
+    loc_add_line_1: faker.address.streetAddress(),
+    loc_add_city: faker.address.city(),
+    loc_add_state: faker.address.state(),
+    loc_add_zip: faker.address.zipCode().split('-')[0],
+  });
+}
+
+exports.seed = function (knex, Promise) {
+  const iterationArray = new Array(100);
+
+  var ArrayOfPromises = Array.from(iterationArray)
+  .map(() => {
+    return addressSeed(knex);
+  });
+
+  return Promise.all(ArrayOfPromises);
+};
