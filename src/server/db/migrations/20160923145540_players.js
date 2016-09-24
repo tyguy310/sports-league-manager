67952ce804d08eb87577fad4ce82c0764a091095
Heathcliff <dennis.nathan@gmail.com>
2016-09-24 11:18:08 -0600

    Created players and sports migrations.

--- src/server/db/migrations/20160923145540_players.js
+++ src/server/db/migrations/20160923145540_players.js
@@ -1,8 +1,19 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('players' (table) => {
+    table.increments();
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.string('email').notNullable();
+    table.string('profile_picture');
+    table.string('username').notNullable();
+    table.string('password').notNullable();
+    table.string('tagline');
+    table.string('zip_code').notNullable();
+    table.integer('availability').defaultTo(36);
+    table.string('gender');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('players');
 };
--- src/server/db/migrations/20160923145552_sports.js
+++ src/server/db/migrations/20160923145552_sports.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('sports', (table) => {
+    table.increments();
+    table.string('type').notNullable();
+    table.integer('team_capacity').notNullable();
+    table.boolean('own_equipment_required').defaultTo(false);
+    table.integer('number_already_registered').notNullable.defaultTo(0);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('sports');
 };
