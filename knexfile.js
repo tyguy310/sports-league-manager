d340c593666daf5257aed53a50ddfcca2824517d
Heathcliff <dennis.nathan@gmail.com>
2016-09-26 09:47:45 -0600

    changed routes from res.render to res.json, added production environment to knexfile

--- knexfile.js
+++ knexfile.js
@@ -21,4 +21,11 @@ module.exports = {
       directory: __dirname + '/src/server/db/seeds'
     }
   }
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+  },
 };
