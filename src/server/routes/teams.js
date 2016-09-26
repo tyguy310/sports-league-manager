1fad6524757a1ef6d35ce6c9b92ba41cc0469752
Heathcliff <dennis.nathan@gmail.com>
2016-09-26 11:29:29 -0600

    created get all and get single routes in teams.js. Updated route config.

---
+++ src/server/routes/teams.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const queries = require('../db/queries');
+
+router.get('/', function (req, res, next) {
+  queries.getItems('teams', function (err, result) {
+    if (err) {
+      res.json({
+        error: err.message || 'An error occurred, please try again.';
+      });
+    } else {
+      res.json({
+        teams: result
+      });
+    }
+  });
+});
+
+router.get('/:id', function (req, res, next) {
+  let teamId = req.params.id;
+  queries.getItems('teams', function (err, result) {
+    if (err) {
+      res.json({
+        error: err.message || 'No team found.';
+      });
+    } else {
+      res.json({
+        team: result
+      });
+    }
+  });
+});
+
+module.exports = router;
