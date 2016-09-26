e759552388b4e50f88e266519759daff895f00a3
Tyler Maier <tyler_maier@yahoo.com>
2016-09-26 12:32:07 -0600

    adds join tables

--- src/server/db/migrations/20160923145609_players_teams.js
+++ src/server/db/migrations/20160923145609_players_teams.js
@@ -1,7 +1,12 @@
 exports.up = function(knex, Promise) {
-
+  return knex.schema.createTable('players_teams', table => {
+    table.integer('players_id').notNullable();
+    table.foreign('players_id').references('id').inTable('players');
+    table.integer('teams_id').notNullable();
+    table.foreign('teams_id').references('id').inTable('teams');
+  });
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('players_teams');
 };
--- src/server/db/migrations/20160923145617_players_sports.js
+++ src/server/db/migrations/20160923145617_players_sports.js
@@ -1,7 +1,12 @@
 exports.up = function(knex, Promise) {
-
+  return knex.schema.createTable('players_sports', table => {
+    table.integer('players_id').notNullable();
+    table.foreign('players_id').references('id').inTable('players');
+    table.integer('sports_id').notNullable();
+    table.foreign('sports_id').references('id').inTable('sports');
+  });
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('players_sports');
 };
