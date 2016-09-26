2df2e9ba6f9026325d4b0fe3acecfcefa8d130a8
Heathcliff <dennis.nathan@gmail.com>
2016-09-26 15:38:40 -0600

    created post route in teams, updated migrations so that teams no longer has a player_id column

--- src/server/routes/teams.js
+++ src/server/routes/teams.js
@@ -46,4 +46,29 @@ router.get('/myteams/:id', function (req, res, next) {
   });
 });
 
+router.post('/', function (req, res, next) {
+  let renderObject = {};
+  let newTeam = {
+    name: req.body.name,
+    image: req.body.image,
+    zip: req.body.zip,
+    gender: req.body.gender,
+    coed: req.body.coed,
+    sports_id: req.body.sports_id
+  };
+  queries.postItem('teams', newTeam, function (err, result) {
+    if (err) {
+      renderObject.message = err.message || 'Sorry, there was an issue creating that team. Please try again.';
+      res.json({
+        error: renderObject
+      });
+    } else {
+      renderObject.player = result;
+      res.json({
+        team: renderObject
+      });
+    }
+  });
+});
+
 module.exports = router;
