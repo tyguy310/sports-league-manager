1224fe2f0aabae1df242cce8bdffe024874e3739
Allen Fordham <allen.s.fordham@gmail.com>
2016-09-28 16:52:58 -0600

    god damn it

--- src/server/db/migrations/20160923145609_players_teams.js
+++ src/server/db/migrations/20160923145609_players_teams.js
@@ -1,8 +1,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('players_teams', table => {
-    // table.integer('players_id').notNullable();
+    table.integer('players_id');
     table.foreign('players_id').references('id').inTable('players');
-    // table.integer('teams_id').notNullable();
+    table.integer('teams_id');
     table.foreign('teams_id').references('id').inTable('teams');
   });
 };
