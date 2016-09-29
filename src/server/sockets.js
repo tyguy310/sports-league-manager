cb67aa1bb705a796bae05dc5b8e84a0c5304aef9
Alex <alnye655321@gmail.com>
2016-09-29 07:07:18 -0600

    small socket.chat server change

--- src/server/sockets.js
+++ src/server/sockets.js
@@ -1,9 +1,20 @@
-const io = require('socket.io')();
-io.on('connection', function(socket) {
+var io = require('socket.io').listen(80);
 
-  socket.on('createMessage', function(data) {
-    socket.emit('postMessage', data);
-  });
+io.sockets.on('connection', function (socket) {
+
+socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
 
+// const io = require('socket.io')();
+// io.on('connection', function(socket) {
+//
+//   socket.on('createMessage', function(data) {
+//     socket.emit('postMessage', data);
+//   });
+// });
+
 module.exports = io;
+//http://stackoverflow.com/questions/10738073/socket-io-phonegap/10886574#10886574
