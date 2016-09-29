9d02b92cd5108a4f81a2c7d38f9b3e4b6de85218
Allen Fordham <allen.s.fordham@gmail.com>
2016-09-29 09:30:51 -0600

    hardcoded in a few team images

--- src/server/db/seeds/05_teams.js
+++ src/server/db/seeds/05_teams.js
@@ -9,10 +9,23 @@ function randomGender() {
   }
 }
 
+function randomPicture() {
+  var picNum = (Math.floor(Math.random() * 4) + 1);
+  if (picNum === 1) {
+    return 'http://www.hicelebration.com/images/landing-pages/groups/sports-teams.jpg';
+  } else if (picNum === 2) {
+    return 'http://www.brandtschool.de/fileadmin/_migrated/pics/Fussballteam.JPG';
+  } else if (picNum === 3) {
+    return 'http://theberrics.com/media/video/cache/1/cached_thumbnail/700x400/af097278c5db4767b0fe9bb92fe21690/cached_thumbnail/p/r/promo280110723.jpg';
+  } else {
+    return 'http://www.cloggingcontest.com/INimages/2013%20Winners/power-outage-overall-amateur-adult-team.jpg';
+  }
+}
+
 function teamsSeed(knex) {
   return knex('teams').insert({
     name: (faker.hacker.ingverb() + ' ' + faker.hacker.noun() + 's'),
-    image: '../../images/teams/teamplaceholder1.jpg',
+    image: randomPicture(),
     zip: (80000 + (Math.floor(Math.random() * 20) + 1)),
     gender: randomGender(),
     coed: faker.random.boolean(),
