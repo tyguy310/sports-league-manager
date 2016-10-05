d1613652ae046b797c2def79e5dfa57fab31cad9
Tyler Maier <tyler_maier@yahoo.com>
2016-10-05 13:21:45 -0600

    ES2015 notation

--- src/server/routes/ladders.js
+++ src/server/routes/ladders.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const queries = require('../db/queries');
 
-router.get('/', function (req, res, next) {
-  let renderObject = {};
-  queries.getItems('ladders', function(err, result) {
+router.get('/', (req, res, next) => {
+  const renderObject = {};
+  queries.getItems('ladders', (err, result) => {
     if (err) {
       renderObject.message = err.message || 'Sorry, we had an issue loading all of our ladders. Please try again.';
       res.json({
@@ -19,9 +19,9 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.get('/:id', function (req, res, next) {
-  let renderObject = {};
-  let itemId = req.params.id;
+router.get('/:id', (req, res, next) => {
+  const renderObject = {};
+  const itemId = req.params.id;
   queries.getItems('ladders', (err, result) => {
     if (err) {
       renderObject.message = err.message || 'Sorry, we had an issue finding that ladder. Please try again.';
@@ -35,9 +35,9 @@ router.get('/:id', function (req, res, next) {
   }, itemId);
 });
 
-router.get('/ladder/:ladder_name', function (req, res, next) {
-  let renderObject = {};
-  let ladderName = req.params.ladder_name;
+router.get('/ladder/:ladder_name', (req, res, next) => {
+  const renderObject = {};
+  const ladderName = req.params.ladder_name;
   queries.joinPlayersToLadder(`${ladderName}`, (err, result) => {
     if (err) {
       renderObject.message = err.message || 'Sorry, we had an issue finding that ladder. Please try again.';
@@ -52,8 +52,8 @@ router.get('/ladder/:ladder_name', function (req, res, next) {
 });
 
 router.post('/ladder/:ladder_name/:player_id', (req, res, next) => {
-  let playerId = req.params.player_id;
-  let ladderName = req.params.ladder_name;
+  const playerId = req.params.player_id;
+  const ladderName = req.params.ladder_name;
 
   queries.ladderPost(ladderName, playerId, (err, result) => {
     if (err) {
@@ -66,12 +66,12 @@ router.post('/ladder/:ladder_name/:player_id', (req, res, next) => {
   });
 });
 
-router.put('/ladder/:ladder_name/:winner_id/:loser_id', function (req, res, next) {
-  let winnerId = req.params.winner_id;
-  let loserId = req.params.loser_id;
-  let ladderName = req.params.ladder_name;
+router.put('/ladder/:ladder_name/:winner_id/:loser_id', (req, res, next) => {
+  const winnerId = req.params.winner_id;
+  const loserId = req.params.loser_id;
+  const ladderName = req.params.ladder_name;
 
-  queries.ladderUpdate(ladderName, winnerId, loserId, function(err, result) {
+  queries.ladderUpdate(ladderName, winnerId, loserId, (err, result) => {
     if (err) {
       res.json({
         error: err
@@ -81,5 +81,23 @@ router.put('/ladder/:ladder_name/:winner_id/:loser_id', function (req, res, next
     }
   });
 });
+
+// delete route - query needs to get rank of targeted deletion, delete, then
+// update all lower ranked players up by 1 rank.
+
+// router.delete('/ladder/:ladder_name/:player_id', (req, res, next) => {
+//   const ladderName = req.params.ladder_name;
+//   const playerId = req.params.player_id;
+//
+//   queries.ladderDelete(ladderName, playerId, (err, result) => {
+//     if (err) {
+//       res.json({
+//         error: err
+//       });
+//     } else {
+//       res.json(result);
+//     }
+//   });
+// });
 
 module.exports = router;
