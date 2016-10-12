dad765e1e3a74e184482b60ebf7137ef30179fdb
Tyler Maier <tyler_maier@yahoo.com>
2016-10-12 11:30:18 -0600

    adds register for ladder routes

--- src/server/db/seeds/10_ladders.js
+++ src/server/db/seeds/10_ladders.js
@@ -5,6 +5,7 @@ exports.seed = function (knex, Promise) {
     return Promise.all([
       knex('ladders').insert({
         type: 'Singles Ping Pong',
+        table_name: 'singles_ping_pong',
         sport_id: 11,
         max_positions: 20
       })
